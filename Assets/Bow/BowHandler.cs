9617cdfcdc35dd2091691b5a7b1b4398dcad7dd2
Fabian Mauz <fmauz@ipb-halle.de>
2021-11-23 21:20:02 +0100

    Add ball system

---
+++ Assets/Bow/BowHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BowHandler : MonoBehaviour{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private Camera camera;
+
+    [SerializeField]
+    private Rigidbody2D arrowRB;
+
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        if (Input.touchCount>0) {
+            Vector3 touchPosition = Input.GetTouch(0).position;
+            touchPosition=camera.ScreenToWorldPoint(touchPosition);
+            arrowRB.position = touchPosition;
+            arrowRB.isKinematic = true;
+        }else {
+            arrowRB.isKinematic = false;
+        }
+        
+    }
+}
